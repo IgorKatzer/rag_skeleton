abb4e3b85bd2b8d0f7b71740e8fcb4513cac59da
IgorKatzer <135774750+IgorKatzer@users.noreply.github.com>
2026-04-10 07:30:50 -0300

    Add Streamlit caching to create_vectorstore function

--- embedding_vectorstore.py
+++ embedding_vectorstore.py
@@ -1,7 +1,8 @@
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+import streamlit as st
 
-
+@st.cache_resource
 def create_vectorstore(chunks):
     
    embed_model = HuggingFaceEmbeddings(model_name='mixedbread-ai/mxbai-embed-large-v1')
